08ba61c732e38f74a7e56db0e7095dbe2f4006ae
Yasuaki Tahira <yasuaki.tahira@gmail.com>
2020-03-15 23:06:54 +0900

    init

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}></View>
+    </View>
+  );
+}
